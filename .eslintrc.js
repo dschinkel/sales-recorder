d5beb5e9e455fac370e9773265ee9ead83c29045
Dave Schinkel <dschinkel@gmail.com>
2026-02-19 14:21:04 -0600

    add some important basic rules to eslint on forcing funciotnal components over classes and forcing the llm hopefully to keep methods and react components smaller

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
       typescript: {},
     },
   },
+
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
     'import/extensions': [
@@ -19,11 +20,52 @@ module.exports = {
       },
     ],
     'no-use-before-define': 'off',
-    'react/function-component-definition': 'off',
     'import/prefer-default-export': 'off',
     'no-unused-vars': 'warn',
     'react/no-array-index-key': 'warn',
     'import/no-extraneous-dependencies': 'off',
     'react/destructuring-assignment': 'off',
   },
+
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      rules: {
+        'max-lines-per-function': [
+          'error',
+          {
+            max: 60,
+            skipBlankLines: true,
+            skipComments: true,
+            IIFEs: true,
+          },
+        ],
+        complexity: ['error', 12],
+        'max-depth': ['error', 4],
+      },
+    },
+    {
+      files: ['**/*.tsx'],
+      rules: {
+        'react/function-component-definition': [
+          'error',
+          {
+            namedComponents: 'function-declaration',
+            unnamedComponents: 'arrow-function',
+          },
+        ],
+        'max-lines-per-function': [
+          'error',
+          {
+            max: 120,
+            skipBlankLines: true,
+            skipComments: true,
+            IIFEs: true,
+          },
+        ],
+        complexity: ['error', 12],
+        'max-depth': ['error', 4],
+      },
+    },
+  ],
 };
